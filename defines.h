68594f5e26814d6f770b4df7de0079dacce38454
Julian Bugariu <julian@bugariu.eu>
2016-08-17 19:44:36 +0200

    documentation

--- defines.h
+++ defines.h
@@ -1,24 +1,27 @@
-/***************************************************************************
-*   Copyright (C) by Doru Julian Bugariu                                  *
-*   julian@bugariu.eu                                                     *
-*                                                                         *
-*   This program is free software; you can redistribute it and/or modify  *
-*   it under the terms of the GNU General Public License as published by  *
-*   the Free Software Foundation; either version 2 of the License, or     *
-*   (at your option) any later version.                                   *
-*                                                                         *
-*   This program is distributed in the hope that it will be useful,       *
-*   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
-*   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
-*   GNU General Public License for more details.                          *
-*                                                                         *
-*   You should have received a copy of the GNU General Public License     *
-*   along with this program; if not, write to the                         *
-*   Free Software Foundation                                              *
-*   51 Franklin Street, Fifth Floor                                       *
-*   Boston, MA 02110-1301, USA                                            *
-*   http://www.fsf.org/about/contact.html                                 *
-***************************************************************************/
+/******************************************************************************//**
+ * @file
+ * @brief Some usefull defines
+ *
+ * @copyright Copyright (C) by Doru Julian Bugariu
+ * julian@bugariu.eu
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the
+ * Free Software Foundation
+ * 51 Franklin Street, Fifth Floor
+ * Boston, MA 02110-1301, USA
+ * http://www.fsf.org/about/contact.html
+ *********************************************************************************/
 
 #ifndef GLICKO_DEFINES_H
 #define GLICKO_DEFINES_H
